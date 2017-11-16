5b3e93a2bbe1121ff0460ea80932862a2c3f78db
allen <q1616163@163.com>
2017-11-16 22:27:01 +0800

    11.16

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -19,6 +19,14 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 @RequestMapping("/user/")
+/*
+定义一个控制器类，这个类必须标有@Controller注解。
+当有@Controller注解的控制器收到一个请求时，
+它会寻找一个合适的handler方法去处理这个请求。
+这就需要控制器通过一个或多个handler映射去把每个请求映射到handler方法。
+为了这样做，一个控制器类的方法需要被@RequestMapping注解装饰，
+使它们成为handler方法。
+ */
 public class UserController {
     @Autowired
     private IUserService iUserService;
@@ -49,11 +57,42 @@ public class UserController {
     @RequestMapping(value = "register.do",method = RequestMethod.GET)
     @ResponseBody
     public ServerResponse<String> register(User user){
+
         return iUserService.register(user);
     }
     @RequestMapping(value = "check_valid.do",method = RequestMethod.GET)
     @ResponseBody
     public  ServerResponse<String > checkValid(String str,String type){
+
         return iUserService.checkValid(str,type);
     }
+    //获取用户信息
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user!=null){
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户的信息");
+    }
+    @RequestMapping(value = "forget_get_question.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> forgetGetQuestion(String username){
+
+        return iUserService.selectQuestion(username);
+    }
+
+    @RequestMapping(value = "forget_check_answer.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username,String question,String answer){
+        return iUserService.checkAnswer(username,question,answer);
+    }
+
+    @RequestMapping(value = "forget_reset_password.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken){
+        return iUserService.forgetRestPassword(username,passwordNew,forgetToken);
+    }
+
 }
