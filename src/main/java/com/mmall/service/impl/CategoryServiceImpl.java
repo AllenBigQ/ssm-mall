4ab1a5665f17fd1bf216fc1264f173590ea6216f
allen <q1616163@163.com>
2017-11-22 15:15:04 +0800

    11.21

--- src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -1,18 +1,29 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.CategoryMapper;
 import com.mmall.pojo.Category;
 import com.mmall.service.ICategoryService;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Allen
  */
 @Service("iCategoryService")
 public class CategoryServiceImpl implements ICategoryService {
+
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
     @Autowired
     private CategoryMapper categoryMapper;
 
@@ -20,13 +31,14 @@ public class CategoryServiceImpl implements ICategoryService {
         if(parentId == null || StringUtils.isBlank(categoryName)){
             return ServerResponse.createByErrorMessage("添加品类参数错误");
         }
+
         Category category = new Category();
         category.setName(categoryName);
-        category.setId(parentId);
+        category.setParentId(parentId);
         category.setStatus(true);//这个分类是可用的
 
         int rowCount = categoryMapper.insert(category);
-        if(rowCount>0){
+        if(rowCount > 0){
             return ServerResponse.createBySuccess("添加品类成功");
         }
         return ServerResponse.createByErrorMessage("添加品类失败");
@@ -45,5 +57,46 @@ public class CategoryServiceImpl implements ICategoryService {
             return ServerResponse.createBySuccess("更新品类名字成功");
         }
         return ServerResponse.createByErrorMessage("更新品类名字失败");
+    }
+
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId){
+        List<Category> categoryList=categoryMapper.selectCategoryChildByParentId(categoryId);
+        if (CollectionUtils.isEmpty(categoryList)){
+            logger.info("未找到当前分类的子分类");
+        }
+        return ServerResponse.createBySuccess(categoryList);
+    }
+
+    /**
+     * 用递归查询本节点的id以及孩子节点的id
+     * @param categoryId
+     * @return
+     */
+    public ServerResponse selectCategoryAndChildrenById(Integer categoryId){
+        Set<Category> categorySet = Sets.newHashSet();  //瓜娃中的sets方法进行初始化
+        findChildCategory(categorySet,categoryId);
+        List<Integer> categoryList = Lists.newArrayList();
+        if(categoryId!=null){
+            for (Category categoryItem:categorySet
+                 ) {
+                categoryList.add(categoryItem.getId());
+            }
+        }
+        return ServerResponse.createBySuccess(categoryList);
+    }
+
+    //递归算法 算出子节点
+    private Set<Category> findChildCategory(Set<Category> categorySet,Integer categoryId){
+        Category category = categoryMapper.selectByPrimaryKey(categoryId);
+        if(category !=null){
+            categorySet.add(category);
+        }
+        //查找子节点，递归算法一定要有一个退出的条件
+        List<Category> categoryList = categoryMapper.selectCategoryChildByParentId(categoryId);
+        for (Category categoryItem:categoryList
+             ) {
+            findChildCategory(categorySet,categoryItem.getId());
+        }
+        return categorySet;
     }
 }
