1d94d0e91b85b962433199cb0f05b5d8a3c779ea
allen <q1616163@163.com>
2017-12-11 15:59:34 +0800

    12.11

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -34,6 +34,17 @@ public class OrderController {
     @Autowired
     private IOrderService iOrderService;
 
+    @RequestMapping("creat.do")
+    @ResponseBody
+    public ServerResponse creat(HttpSession session, Long orderNo, HttpServletRequest request){
+        User user =(User) session.getAttribute(Const.CURRENT_USER);
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+
+        return iOrderService.pay(orderNo,user.getId(),path);
+    }
+
     @RequestMapping("pay.do")
     @ResponseBody
     public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request){
